f189c4f7c25f88f975992c6c25c7746228ee005e
jmdaugherty94@gmail.com <jmdaugherty94@gmail.com>
2021-07-28 16:17:58 -0400

    Add project files.

---
+++ Workoutselector/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Workoutselector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random r = new Random();
+
+            int reps1 = r.Next(15, 25);
+            int reps2 = r.Next(15, 25);
+            int reps3 = r.Next(15, 25);
+            int rounds = r.Next(3, 6);
+
+            Console.WriteLine($"Workout will be {rounds} rounds of: ");
+            Console.WriteLine($"Push-ups:{reps1} reps");
+            Console.WriteLine($"Sit-ups:{reps2} reps");
+            Console.WriteLine($"Air-Squats:{reps3} reps");
+        }
+    }
+}
